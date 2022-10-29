65fed5153cd956277ad39b02086d0532784e5a28
Madina Mel <wolfi251199@gmail.com>
2022-10-30 00:24:18 +0200

    Added new lesson

---
+++ Lesson_09/Classwork/scripts/script.js
@@ -0,0 +1,40 @@
+// let arr = [0, 1, 2, 3, 4];
+
+// let arr_spliced = arr.splice(2);
+
+// console.log(arr);  // [0, 1]
+// console.log(arr_spliced);   // [2, 3, 4]
+
+
+
+// Дан массив с числами. Найдите сумму элементов до первого нуля. Пример: [1, 2, 3, 0, 4, 5, 6] - 
+// суммируем первые 3 элемента, так как дальше стоит элемент с числом 0.
+
+let array = [0, 34, 4, 1, 3, 5, 0, 9, 5, 0, 1];
+
+// let sum = 0;
+let result = array.reduce((total, el, _, arr) => {
+    if (el === 0) {
+        arr.splice(0);
+    } 
+    return total + el;
+    
+}, 0);
+
+//без 0 -> total == 0 (из массива), el == 34
+//c 0 -> total == 0 (из аргумента), el == 0
+
+console.log(result);
+
+
+
+
+let arr1 = [1, 2, 3];
+let sum = arr1.reduce((total, el) => {
+    return total + el;
+}, 0);
+
+// c числом -> total == число, el == первый элемент массива
+// без числа -> total == первое число массива, el == второй элемент массива
+
+console.log(sum);
