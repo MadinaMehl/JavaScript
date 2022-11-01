bea88e0b5d584ce21b023e80ca8a85bd8f7ec80f
Madina Mel <wolfi251199@gmail.com>
2022-11-01 13:43:31 +0100

    Added homework

--- Lesson_10/Homework/scripts/script.js
+++ Lesson_10/Homework/scripts/script.js
@@ -35,7 +35,20 @@ const people = [
 // 1. Сформируйте массив строк - каждая строка включает в себя информацию по каждому человеку 
 // в следующем формате: "<Имя> <Фамилия> (<Город>, <Страна>)". Пример => ['Ivan Ivanov (Moscow, Russia)', ...]
 
+
+let userInfo = people.map(el => `${el.first_name} ${el.last_name} (${el.city}, ${el.country})`);
+console.log(userInfo);
+
+
 //2. Сформируйте массив из людей, чьи имена начинаются на 'I' (регистр буквы может быть и нижним)
 
+let names = people.filter(el => el.first_name[0].toUpperCase() === 'I');
+console.log(names);
+
+
+
 //3. Получите из массива объект, где страной проживания человека является 'Germany' 
+
+let country = people.find(el => el.country === 'Germany');
+console.log(country);
 
